78593229e9755be09e43f7fcfc14d01fc170f9c6
nindy wahyu <90910271+nindywahyu@users.noreply.github.com>
2021-12-13 19:35:01 +0700

    Add files via upload

---
+++ daftarorder.php
@@ -0,0 +1,542 @@
+<?php
+session_start();
+include 'dbconnect.php';
+
+if(!isset($_SESSION['log'])){
+	header('location:login.php');
+} else {
+	
+};
+	
+	$uid = $_SESSION['id'];
+	$caricart = mysqli_query($conn,"select * from cart where userid='$uid' and status='Cart'");
+	$fetc = mysqli_fetch_array($caricart);
+	$orderidd = $fetc['orderid'];
+	$itungtrans = mysqli_query($conn,"select count(orderid) as jumlahtrans from cart where userid='$uid' and status!='Cart'");
+	$itungtrans2 = mysqli_fetch_assoc($itungtrans);
+	$itungtrans3 = $itungtrans2['jumlahtrans'];
+	
+if(isset($_POST["update"])){
+	$kode = $_POST['idproduknya'];
+	$jumlah = $_POST['jumlah'];
+	$q1 = mysqli_query($conn, "update detailorder set qty='$jumlah' where idproduk='$kode' and orderid='$orderidd'");
+	if($q1){
+		echo "Berhasil Update Cart
+		<meta http-equiv='refresh' content='1; url= cart.php'/>";
+	} else {
+		echo "Gagal update cart
+		<meta http-equiv='refresh' content='1; url= cart.php'/>";
+	}
+} else if(isset($_POST["hapus"])){
+	$kode = $_POST['idproduknya'];
+	$q2 = mysqli_query($conn, "delete from detailorder where idproduk='$kode' and orderid='$orderidd'");
+	if($q2){
+		echo "Berhasil Hapus";
+	} else {
+		echo "Gagal Hapus";
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>HIGH n JAR' - Daftar Belanja</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tokopekita, Richard's Lab" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<link href='//fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+<style>
+	.navbar-default {
+    background: none;
+	background-color: #d1b398;
+    border: none;
+    text-align: center;}
+
+	.navigation-agileits {
+	background-color: #d1b398 !important;}
+
+	.w3_footer_grid ul.address li i {
+    color: #e6d9c9;
+    border: 1px solid #e6d9c9;
+    padding: 0.5em;
+    margin-right: 1em;
+	}
+
+	button.btn.btn-default.search {
+    background: #d1b398;
+    padding: 11px 12px 11px;
+    float: right;
+    outline: none;
+    border: none;
+	}
+
+	html, body{
+    font-size: 100%;
+	font-family: 'Open Sans', sans-serif;
+	background:#ece7deee;
+	}
+
+	.breadcrumbs {
+    padding: 1.5em 0;
+    background: #ece7deee;
+	}
+	.breadcrumb1 li a {
+    color: #d0b298;
+    text-decoration: none;
+	}
+	.breadcrumb {
+    padding: 8px 15px;
+    margin-bottom: 20px;
+    list-style: none;
+    background-color: #ece7deee;
+    border-radius: 4px;}
+
+	.top-brands h2:after, .newproducts-w3agile h3:after, .login h2:after, .register h2:after, .faq-w3agile h3:after, .brands h3:after {
+    content: '';
+    background: #d0b298;
+    height: 2px;
+    width: 15%;
+    position: absolute;
+    bottom: 0%;
+    left: 43%;
+	}
+
+	.snipcart-details input.button {
+	font-size: 14px;
+    color: #fff;
+    background: #d0b298;
+    text-decoration: none;
+    position: relative;
+    border: none;
+    border-radius: 0;
+    width: 100%;
+    text-transform: uppercase;
+    padding: 0.5em 0;
+    outline: none;}
+
+	.agile_top_brand_left_grid:hover .snipcart-details input.button,.snipcart-details input.button:hover{
+    background: #e6d9c9;
+	}
+
+	.w3_footer_grid ul.address li i {
+    color: #e6d9c9;
+    border: 1px solid #e6d9c9;
+    padding: 0.5em;
+    margin-right: 1em;}
+
+	.ul.multi-column-dropdown h6 {
+    font-size: 20px;
+    color: #d0b298;
+    margin: 0 0 0em;
+    padding-bottom: 1em;
+    border-bottom: 1px solid #E4E4E4;
+    text-transform: capitalize;}
+
+	.snipcart-details input.button {
+    font-size: 14px;
+    color: #fff;
+    background: #d0b298;
+    text-decoration: none;
+    position: relative;
+    border: none;
+    border-radius: 0;
+    width: 100%;
+    text-transform: uppercase;
+    padding: 0.5em 0;
+    outline: none;
+	}
+
+	.w3ls_logo_products_left h1 a {
+    font-size: 40px;
+    color: #d1b398;
+    text-decoration: none;
+    text-transform: uppercase;
+    display: block;
+    line-height: 1;
+	}
+
+	.login p a {
+    color: #d1b398;
+    text-decoration: none;
+    font-size: 1.2em;
+    padding: 0 0.5em;
+	}
+
+	.login p a {
+    color: #d1b398;
+    text-decoration: none;
+    font-size: 1.2em;
+    padding: 0 0.5em;
+	}
+
+	.login-form-grids input[type="submit"] {
+    outline: none;
+    border: none;
+    padding: 10px 0;
+    font-size: 1em;
+    color: #fff;
+    display: block;
+    width: 100%;
+    background: #d1b398;
+    margin: 1.5em 0 0;}
+
+	.login-form-grids input[type="submit"]:hover{
+	background:#e6d9c9};
+
+	.register-home a {
+    padding: 8px 45px;
+    background: #e6d9c9;
+    color: #fff;
+    font-size: 1em;
+    text-decoration: none;	}
+
+	.register-home a{
+	padding: 8px 45px;
+    background: #e6d9c9;
+	color:#fff;
+	font-size:1em;
+	text-decoration:none;}
+
+	.register-home a:hover{
+	background:#d1b398;}
+
+	.timetable_sub th {
+    background: #e6d9c9;
+    color: #fff !important;
+    text-transform: capitalize;
+    font-size: 13px;
+    border-right: 1px solid #e6d9c9;
+	}
+
+	.checkout-left-basket h4 {
+    padding: 1em;
+    background: #d1b398;
+    font-size: 1.1em;
+    color: #fff;
+    text-transform: uppercase;
+    text-align: center;
+    margin: 0 0 1em;
+	}
+
+	.checkout h2 span {
+    color: #d1b398;
+	}
+
+
+</style>	
+<body>
+<!-- header -->
+	<div class="agileits_header">
+		<div class="container">
+			<div class="agile-login">
+				<ul>
+				<?php
+				if(!isset($_SESSION['log'])){
+					echo '
+					<li><a href="registered.php"> Daftar</a></li>
+					<li><a href="login.php">Masuk</a></li>
+					';
+				} else {
+					
+					if($_SESSION['role']=='Member'){
+					echo '
+					<li style="color:white">Halo, '.$_SESSION["name"].'
+					<li><a href="logout.php">Keluar?</a></li>
+					';
+					} else {
+					echo '
+					<li style="color:white">Halo, '.$_SESSION["name"].'
+					<li><a href="admin">Admin Panel</a></li>
+					<li><a href="logout.php">Keluar?</a></li>
+					';
+					};
+					
+				}
+				?>
+					
+				</ul>
+			</div>
+			<div class="product_list_header">  
+					<a href="cart.php"><button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
+					 </a>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+
+	<div class="logo_products">
+		<div class="container">
+		<div class="w3ls_logo_products_left1">
+				<ul class="phone_email">
+					<li><i class="fa fa-phone" aria-hidden="true"></i>Hubungi Kami : (+62) 812 3456 78993</li>
+				</ul>
+			</div>
+			<div class="w3ls_logo_products_left">
+				<h1><a href="index.php">HIGH n JAR'</a></h1>
+			</div>
+		<div class="w3l_search">
+			<form action="search.php" method="post">
+				<input type="search" name="Search" placeholder="Cari produk...">
+				<button type="submit" class="btn btn-default search" aria-label="Left Align">
+					<i class="fa fa-search" aria-hidden="true"> </i>
+				</button>
+				<div class="clearfix"></div>
+			</form>
+		</div>
+			
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //header -->
+<!-- navigation -->
+	<div class="navigation-agileits">
+		<div class="container">
+			<nav class="navbar navbar-default">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header nav_2">
+								<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div> 
+							<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="index.php" class="act">Home</a></li>	
+									<!-- Mega Menu -->
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Kategori Produk<b class="caret"></b></a>
+										<ul class="dropdown-menu multi-column columns-3">
+											<div class="row">
+												<div class="multi-gd-img">
+													<ul class="multi-column-dropdown">
+														
+														
+														<?php 
+														$kat=mysqli_query($conn,"SELECT * from kategori order by idkategori ASC");
+														while($p=mysqli_fetch_array($kat)){
+
+															?>
+														<li><a href="kategori.php?idkategori=<?php echo $p['idkategori'] ?>"><?php echo $p['namakategori'] ?></a></li>
+																				
+														<?php
+																	}
+														?>
+													</ul>
+												</div>	
+												
+											</div>
+										</ul>
+									</li>
+									<li><a href="cart.php">Keranjang Saya</a></li>
+									<!-- <li><a href="daftarorder.php">Daftar Order</a></li> -->
+								</ul>
+							</div>
+							</nav>
+			</div>
+		</div>
+		
+<!-- //navigation -->
+<!-- breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<ol class="breadcrumb breadcrumb1">
+				<li><a href="index.php">Home</a></li>
+				<li class="active">Checkout</li>
+			</ol>
+		</div>
+	</div>
+<!-- //breadcrumbs -->
+<!-- checkout -->
+	<div class="checkout">
+		<div class="container">
+			<h2>Kamu memiliki <span><?php echo $itungtrans3 ?> transaksi</span></h2>
+			<div class="checkout-right">
+				<table class="timetable_sub">
+					<thead>
+						<tr>
+							<th>No.</th>	
+							<th>Kode Order</th>
+							<th>Tanggal Order</th>
+							<th>Total</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					
+					<?php 
+					
+						$brg=mysqli_query($conn,"SELECT DISTINCT(idcart), c.orderid, tglorder, status from cart c, detailorder d where c.userid='$uid' and d.orderid=c.orderid and status!='Cart' order by tglorder DESC");
+						$no=1;
+						while($b=mysqli_fetch_array($brg)){
+
+					?>
+					<tr class="rem1"><form method="post">
+						<td class="invert"><?php echo $no++ ?></td>
+						<td class="invert"><a href="order.php?id=<?php echo $b['orderid'] ?>"><?php echo $b['orderid'] ?></a></td>
+						
+						<td class="invert"><?php echo $b['tglorder'] ?></td>
+						<td class="invert">
+						
+						Rp<?php 				$ongkir = 10000;
+												$ordid = $b['orderid'];
+												$result1 = mysqli_query($conn,"SELECT SUM(qty*hargaafter)+$ongkir AS count FROM detailorder d, produk p where d.orderid='$ordid' and p.idproduk=d.idproduk order by d.idproduk ASC");
+												$cekrow = mysqli_num_rows($result1);
+												$row1 = mysqli_fetch_assoc($result1);
+												$count = $row1['count'];
+												if($cekrow > 0){
+													echo number_format($count);
+													} else {
+														echo 'No data';
+													}?>
+						
+						</td>
+				
+						<td class="invert">
+							<div class="rem">
+								<?php
+								if($b['status']=='Payment'){
+								echo '
+								<a href="konfirmasi.php?id='.$b['orderid'].'" class="form-control btn-primary">
+								Konfirmasi Pembayaran
+								</a>
+								';}
+								else if($b['status']=='Diproses'){
+								echo 'Pesanan Diproses (Pembayaran Diterima)';
+								}
+								else if($b['status']=='Dikirim'){
+									echo 'Pesanan Dikirim';
+								} else if($b['status']=='Selesai'){
+									echo 'Pesanan Selesai';
+								} else if($b['status']=='Dibatalkan'){
+									echo 'Pesanan Dibatalkan';
+								} else {
+									echo 'Konfirmasi diterima';
+								}
+								
+								?>
+							</form>
+							</div>
+							<script>$(document).ready(function(c) {
+								$('.close1').on('click', function(c){
+									$('.rem1').fadeOut('slow', function(c){
+										$('.rem1').remove();
+									});
+									});	  
+								});
+						   </script>
+						</td>
+					</tr>
+					<?php
+						}
+					?>
+					
+								<!--quantity-->
+									<script>
+									$('.value-plus').on('click', function(){
+										var divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.text(), 10)+1;
+										divUpd.text(newVal);
+									});
+
+									$('.value-minus').on('click', function(){
+										var divUpd = $(this).parent().find('.value'), newVal = parseInt(divUpd.text(), 10)-1;
+										if(newVal>=1) divUpd.text(newVal);
+									});
+									</script>
+								<!--quantity-->
+				</table>
+			</div>
+		</div>
+	</div>
+<!-- //checkout -->
+<!-- //footer -->
+<div class="footer">
+		<div class="container">
+			<div class="w3_footer_grids">
+				<div class="col-md-4 w3_footer_grid">
+					<h3>Hubungi Kami</h3>
+					
+					<ul class="address">
+						<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>DKI Jakarta.</li>
+						<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:info@email">info@email</a></li>
+						<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>(+62) 812 3456 7899</li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		
+		<div class="footer-copy">
+			
+			<div class="container">
+				<p>© 2021 HIGH n JAR'</p>
+			</div>
+		</div>
+		
+	</div>	
+		</div>
+<!-- //footer -->	
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- top-header and slider -->
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 4000,
+				easingType: 'linear' 
+				};
+			
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+
+<!-- main slider-banner -->
+<script src="js/skdslider.min.js"></script>
+<link href="css/skdslider.css" rel="stylesheet">
+<script type="text/javascript">
+		jQuery(document).ready(function(){
+			jQuery('#demo1').skdslider({'delay':5000, 'animationSpeed': 2000,'showNextPrev':true,'showPlayButton':true,'autoSlide':true,'animationType':'fading'});
+						
+			jQuery('#responsive').change(function(){
+			  $('#responsive_wrapper').width(jQuery(this).val());
+			});
+			
+		});
+</script>	
+<!-- //main slider-banner --> 
+</body>
+</html>
